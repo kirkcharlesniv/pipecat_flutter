4ce1efe1366ec7225391391ae77d3d76318dd61c
kirkcharlesniv <kirkniverba@icloud.com>
2026-02-01 02:35:27 +0800

    refactor: temporarily remove tests for now

--- pipecat_flutter/lib/pipecat_flutter.dart
+++ pipecat_flutter/lib/pipecat_flutter.dart
@@ -1,10 +1,3 @@
 import 'package:pipecat_flutter_platform_interface/pipecat_flutter_platform_interface.dart';
 
 PipecatFlutterPlatform get _platform => PipecatFlutterPlatform.instance;
-
-/// Returns the name of the current platform.
-Future<String> getPlatformName() async {
-  final platformName = await _platform.getPlatformName();
-  if (platformName == null) throw Exception('Unable to get platform name.');
-  return platformName;
-}
